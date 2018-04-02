aec909074a2969f2e7177d9192903ccb220a7ff3
Ethan Brown <ebrown@mmm.com>
2018-04-02 12:08:57 -0600

    exceptionQuine.py

---
+++ Python/exceptionQuine.py
@@ -0,0 +1,4 @@
+    File "c:/Users/a7gtqzz/Projects/HackerRank/Python/exceptionQuine.py", line 1
+        File "c:/Users/a7gtqzz/Projects/HackerRank/Python/exceptionQuine.py", line 1
+        ^
+IndentationError: unexpected indent
