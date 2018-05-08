381931a021b977e7f9cdaf7e535e264c8acd12c2
Ethan Brown <ebrown@mmm.com>
2018-05-08 09:55:12 -0600

    constructing a number, math challenge

---
+++ Python/Math/number_theory/constructing_a_number.py
@@ -0,0 +1,14 @@
+# https://www.hackerrank.com/challenges/constructing-a-number/problem
+import sys
+
+def canConstruct(a):
+    return 'Yes' if sum(a)%3 == 0 else 'No'
+
+if __name__ == "__main__":
+    t = int(raw_input().strip())
+    for a0 in xrange(t):
+        n = int(raw_input().strip())
+        a = map(int, raw_input().strip().split(' '))
+        result = canConstruct(a)
+        print result
+        
