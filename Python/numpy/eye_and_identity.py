4c2860b86c0c1067bc2e8769b5ff51dd2ace866a
Ethan Brown <embrown801@gmail.com>
2018-05-23 09:50:01 -0600

    finished list comprehensions challenge

--- Python/numpy/eye_and_identity.py
+++ Python/numpy/eye_and_identity.py
@@ -1,7 +1,6 @@
 # https://www.hackerrank.com/challenges/np-eye-and-identity/problem
 import numpy
 numpy.set_printoptions(sign=' ')
-n, m = raw_input().split()
-n = int(n)
-m = int(m)
+n, m = map(int, raw_input().split())
+
 print numpy.eye(n, m)
