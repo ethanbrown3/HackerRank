016bd9abc12c5a81b6d058aa146a8e6dac9f14f4
Ethan Brown <ebrown@adobe.com>
2018-10-21 16:02:03 -0600

    prime factors python

---
+++ Python/Math/fundamentals/leonardos_prime_factors.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+prime=[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+for i in range(int(input())):
+    count = 0
+    result = 1
+    n=int(input())
+    for j in prime:
+        result *= j
+        if(result <= n):
+            count += 1
+    print(count)
